8222d52e260780e569364b7447faf1e5161e6559
jumpei28 <k24074kk@aitech.ac.jp>
2026-01-08 15:34:52 +0900

    wage_manager.py追加debugした

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import shift_manager
-import wage_manager
+from routes import shift_manager, wage_manager
 
 app = Flask(__name__)
 app.secret_key = "secret_key_for_session"
